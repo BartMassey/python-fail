718b5d7b0841db6d1f3d1aa7abd9fb889ffcfd37
Bart Massey <bart.massey@gmail.com>
2020-10-20 14:46:21 -0700

    updated scopefail and added README

---
+++ scope-fail/README.md
@@ -0,0 +1,11 @@
+# Python Scope Fail
+Copyright © 2014 Bart Massey
+
+Python's scoping and variable creation rules are a little
+strange: see the code for details.
+
+-----
+
+This work is licensed under the "MIT License".  Please see
+the file COPYING in the source distribution of this software
+for license terms.
--- scope-fail/scopefail.py
+++ scope-fail/scopefail.py
@@ -6,7 +6,12 @@
 x = 3
 
 def f():
+    # The `x` here refers to the local variable *defined on
+    # the next line*!  It thus is "referenced before
+    # assignment".
     print(x)
+    # This creates a new local `x` that shadows the global
+    # `x`. It does not alter the global `x`.
     x = 4
     print(x)
 
